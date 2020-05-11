5c7ceda234d0cc1e7c1349382099207298e3f83d
maheshkumars <mahesh.sahu@innoventes.co>
2020-05-11 13:23:58 +0530

    comminting landing page

---
+++ src/components/JoinPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { 
+    IonApp, 
+    IonImg,
+    IonContent
+ } from '@ionic/react';
+
+const JoinPage: React.FC=()=>(
+   <IonApp>
+       <IonContent>
+          <div>Join Page</div>
+       </IonContent>
+   </IonApp>
+)
+
+export default JoinPage;
---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import './LandingPage.css'
+import { logoFacebook} from 'ionicons/icons';
+import { 
+    IonApp, 
+    IonImg,
+    IonContent,
+    IonIcon,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonTitle,
+    IonText,
+    IonButton
+ } from '@ionic/react';
+
+const LandingPage: React.FC<{pic:string}>=props=>(
+   <IonApp>
+       <IonContent className="bg-img"> 
+       <IonGrid>
+           <IonRow>
+               <IonCol>
+                 <IonImg src={props.pic} alt="topPic" className="top-img"/>
+                </IonCol>
+            </IonRow>    
+          <IonRow>
+              <IonCol className="ion-padding">
+                 <IonTitle className="vault-txt"> <p> VAULT29</p> </IonTitle>
+              </IonCol>
+        </IonRow>
+        <IonRow>
+            <IonCol className="ion-padding">
+                <IonText className="txt-title"><p>Connecting the world of wine through experiences</p></IonText>
+            </IonCol>
+        </IonRow>
+
+        <div className="btn-background">  
+                <IonRow>
+                    <IonCol> 
+                        <IonButton fill="outline" expand="block" className="btn-facebook" size="large" strong>
+                         <IonIcon icon={logoFacebook} slot="start"></IonIcon>
+                            CONNECT WITH FACEBOOK</IonButton>
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol>
+                        <IonButton fill="outline" expand="block" className="btn-join" size="large" strong>JOIN</IonButton>
+                    </IonCol>
+                    <IonCol>
+                        <IonButton fill="outline" expand="block" className="btn-login" size="large" strong>LOG IN</IonButton>
+                    </IonCol>
+                </IonRow>
+            </div>
+
+      </IonGrid>
+      
+       </IonContent>
+   </IonApp>
+)
+
+export default LandingPage;
