b5d5cc5f5cc9fa4bddc789c1301af7e636bf2985
Mahesh Kumar Sahu <mahesh.sahu@innoventes.co>
2020-05-12 04:49:24 +0000

    Resolve "Landing Page"

---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { 
+    IonApp, 
+    IonImg,
+    IonContent
+ } from '@ionic/react';
+
+const LoginPage: React.FC=()=> {
+return (
+   <IonApp>
+       <IonContent>
+          <div>Login Page</div>
+       </IonContent>
+   </IonApp>
+);
+};
+
+export default LoginPage;
