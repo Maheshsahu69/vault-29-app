e6012ae5975ab58381234c108e029958c73513ba
Mahesh Kumar Sahu <mahesh.sahu@innoventes.co>
2020-05-22 04:34:21 +0000

    Resolve "Join page"

--- src/components/JoinPage.tsx
+++ src/components/JoinPage.tsx
@@ -1,16 +1,221 @@
-import React from 'react';
-import { 
-    IonApp, 
-    IonImg,
-    IonContent
- } from '@ionic/react';
+import React, { useState } from "react";
+import "./JoinPage.css";
+import {
+  IonImg,
+  IonContent,
+  IonPage,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonTitle,
+  IonInput,
+  IonItem,
+  IonButton,
+  IonToast,
+} from "@ionic/react";
+import icon from "../Images/consumer-inactive.png";
 
-const JoinPage: React.FC=()=>(
-   <IonApp>
-       <IonContent>
-          <div>Join Page</div>
-       </IonContent>
-   </IonApp>
-)
+const JoinPage: React.FC = (props: any) => {
+  const [userNameText, setUserNameText] = useState("");
+  const [nameText, setNameText] = useState("");
+  const [emailText, setEmailText] = useState("");
+  const [passwordText, setPasswordText] = useState("");
+  const [confirmPasswordText, setConfirmPasswordText] = useState("");
+
+  const [validationStatus, setValidationStatus] = useState(false);
+  const [validationErrorMessage, setValidationErrorMessage] = useState("");
+
+  const joinClick = async (props: any) => {
+    const state = {
+      username: userNameText,
+      business_name: nameText,
+      email: emailText,
+      user_type: "Consumer",
+      account_type: "Email",
+      password: passwordText,
+    };
+    console.log("state", state);
+    let result = await fetch(
+      "https://vault29-backend.innoventestech.in/v2/user/signup",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(state),
+      }
+    );
+    let res = await result.json();
+    console.log("response", res);
+
+    if (res.success === "true") {
+      props.history.push("/login");
+    } else {
+      if (res.message.username) {
+        if (res.message.username.length > 0) {
+          setValidationStatus(true);
+          setValidationErrorMessage(res.message.username);
+        }
+      }
+
+      if (res.message) {
+        if (res.message === "Email with Account Type Already Registered") {
+          setValidationStatus(true);
+          setValidationErrorMessage(res.message);
+        }
+      }
+
+      if (passwordText !== confirmPasswordText) {
+        setValidationStatus(true);
+        setValidationErrorMessage("Password Mismatch.");
+      }
+      if (passwordText === "") {
+        setValidationStatus(true);
+        setValidationErrorMessage("Password mismatch.");
+      }
+
+      if (res.message.password) {
+        if (res.message.password.length > 0) {
+          setValidationStatus(true);
+          setValidationErrorMessage(res.message.password);
+        }
+      }
+
+      if (res.message.email) {
+        if (res.message.email.length > 0) {
+          setValidationStatus(true);
+          setValidationErrorMessage(res.message.email);
+        }
+      }
+    }
+  };
+  return (
+    <div>
+      <IonPage>
+        <IonContent className="bg-img-join">
+          <IonGrid>
+            <IonRow>
+              <IonCol className="ion-text-center">
+                <IonImg src={icon} alt="topImgJoin" className="top-img-join" />
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol className="ion-padding">
+                <IonTitle className="ion-text-center" id="add-photo-txt">
+                  Add Photo
+                </IonTitle>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <IonItem color="transparent" className="name-item">
+                  <IonInput
+                    placeholder="Name"
+                    className="name-txtbx"
+                    type="text"
+                    onIonChange={(e) => setNameText(e.detail.value!)}
+                    value={nameText}
+                  />
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <IonItem color="transparent" className="username-item">
+                  <IonInput
+                    placeholder="Username"
+                    className="username-txtbx"
+                    type="text"
+                    onIonChange={(e) => setUserNameText(e.detail.value!)}
+                    value={userNameText}
+                  ></IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <IonItem color="transparent" className="email-item">
+                  <IonInput
+                    placeholder="Email Address"
+                    className="email-txtbx"
+                    type="email"
+                    onIonChange={(e) => {
+                      setEmailText(e.detail.value!);
+                    }}
+                    value={emailText}
+                  ></IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <IonItem color="transparent" className="password-item">
+                  <IonInput
+                    placeholder="Password"
+                    className="password-txtbx"
+                    type="password"
+                    onIonChange={(e) => setPasswordText(e.detail.value!)}
+                    value={passwordText}
+                  />
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <IonItem color="transparent" className="confirm-password-item">
+                  <IonInput
+                    placeholder="Confirm Password"
+                    className="confirm-password-txtbx"
+                    type="password"
+                    onIonChange={(e) => setConfirmPasswordText(e.detail.value!)}
+                    value={confirmPasswordText}
+                  ></IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <IonItem color="transparent" className="location-item">
+                  <IonInput
+                    placeholder="Location"
+                    className="location-txtbx"
+                    type="text"
+                  ></IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <IonButton
+                  size="large"
+                  expand="block"
+                  strong
+                  fill="outline"
+                  className="btn-join-joinpage"
+                  onClick={() => joinClick({ ...props })}
+                >
+                  {" "}
+                  JOIN
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonContent>
+      </IonPage>
+
+      {validationErrorMessage && (
+        <IonToast
+          isOpen={validationStatus}
+          onDidDismiss={() => setValidationStatus(false)}
+          message={validationErrorMessage}
+          position="top"
+          duration={2000}
+          color="light"
+        />
+      )}
+    </div>
+  );
+};
 
 export default JoinPage;
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,18 +1,161 @@
-import React from 'react';
-import { 
-    IonApp, 
-    IonImg,
-    IonContent
- } from '@ionic/react';
+import React, { useState } from "react";
+import topPic from "../Images/icon.png";
+import "./LoginPage.css";
+import {
+  IonImg,
+  IonContent,
+  IonPage,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonTitle,
+  IonItem,
+  IonInput,
+  IonButton,
+  IonToast,
+} from "@ionic/react";
+import { Link } from "react-router-dom";
 
-const LoginPage: React.FC=()=> {
-return (
-   <IonApp>
-       <IonContent>
-          <div>Login Page</div>
-       </IonContent>
-   </IonApp>
-);
+const LoginPage: React.FC = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [validationStatus, setValidationStatus] = useState(false);
+  const [validationMessage, setValidationMessage] = useState("");
+  const login = async (props: any, password: any, email: any) => {
+    console.log("login click props : ", props);
+    const state = { email: email, password: password };
+    console.log("login state", state);
+    let result = await fetch(
+      "https://vault29-backend.innoventestech.in/v2/user/signin",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(state),
+      }
+    );
+
+    let response = await result.json();
+    console.log("response", response);
+    localStorage.setItem("token", JSON.stringify(response.token));
+    const token = localStorage.getItem("token");
+
+    const successVar = response.success;
+    if (successVar === "true") {
+      props.history.push("/winewall");
+    } else {
+      if (response.message.email) {
+        if (response.message.email.length > 0) {
+          setValidationStatus(true);
+          setValidationMessage(response.message.email);
+        }
+      }
+
+      if (response.message === "Email id is not registered") {
+        setValidationStatus(true);
+        setValidationMessage(response.message);
+      }
+
+      if (response.message === "Invalid Password, Case Sensitive") {
+        setValidationStatus(true);
+        setValidationMessage(response.message);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <IonPage>
+        <IonContent className="bg-img-login">
+          <IonGrid>
+            <IonRow>
+              <IonCol className="ion-padding">
+                <IonImg src={topPic} alt="topPic" className="top-img-login" />
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol className="ion-padding">
+                <IonTitle className="login-txt">LOG IN</IonTitle>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol className="ion-padding">
+                <IonItem className="email-ion-item" color="transparent">
+                  <IonInput
+                    type="email"
+                    className="email-txt"
+                    placeholder="Email Address"
+                    onIonChange={(e) => setEmail(e.detail.value!)}
+                    value={email}
+                  />
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol className="ion-padding">
+                <IonItem className="pass-ion-item" color="transparent">
+                  <IonInput
+                    type="password"
+                    className="password-login"
+                    placeholder="Password"
+                    onIonChange={(e) => setPassword(e.detail.value!)}
+                    value={password}
+                  />
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol className="ion-padding">
+                <div className="div-btn">
+                  <IonButton
+                    fill="outline"
+                    expand="block"
+                    strong
+                    size="large"
+                    className="btn-login"
+                    onClick={() => login({ ...props }, password, email)}
+                  >
+                    LOG IN
+                  </IonButton>
+                </div>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol className="ion-padding">
+                <IonTitle className="dash-txt bold">___</IonTitle>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol className="ion-padding">
+                <IonTitle className="new-member-txt"> New member?</IonTitle>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol className="ion-padding">
+                <Link to="/join" className="link-create-account">
+                  <IonTitle className="create-account-txt bold">
+                    CREATE ACCOUNT
+                  </IonTitle>
+                </Link>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonContent>
+      </IonPage>
+      {validationMessage && (
+        <IonToast
+          isOpen={validationStatus}
+          onDidDismiss={() => setValidationStatus(false)}
+          message={validationMessage}
+          duration={2000}
+          position="top"
+          color="light"
+        />
+      )}
+    </div>
+  );
 };
 
 export default LoginPage;
