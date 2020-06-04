aa4ffc7db472dff48b6b1ef2ee5e5453a4a65946
Raghuram <raghu@innoventes.co>
2020-06-04 12:56:28 +0530

    More styling

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import "./Join.css";
+import React, { useState } from 'react';
+import './Join.css';
 import {
   IonImg,
   IonContent,
@@ -16,25 +16,28 @@ import {
   IonLabel,
   IonFooter,
   IonToolbar
-} from "@ionic/react";
-import icon from "../images/consumer-inactive.png";
-import { useHistory } from "react-router";
-import { useDispatch, useSelector } from "react-redux";
-import { doJoin } from "../actions/auth";
-import { JoinForm } from "../types";
-import { UserType, AccountType } from "../constants";
-import { setAlert } from "../actions/alert";
-import { RootState } from "../reducers";
+} from '@ionic/react';
+import icon from '../images/consumer-inactive.png';
+import { useHistory } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { doJoin } from '../actions/auth';
+import { JoinForm } from '../types';
+import { UserType, AccountType } from '../constants';
+import { setAlert } from '../actions/alert';
+import { RootState } from '../reducers';
 
 const Join: React.FC = () => {
   const history = useHistory();
   const dispatch = useDispatch();
 
-  const [username, setUsername] = useState("");
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [gender, setGender] = useState('');
+  const [maleFill, setMaleFill] = useState<"clear" | "outline" | "solid" | "default">('outline');
+  const [femaleFill, setFemaleFill] = useState<"clear" | "outline" | "solid" | "default">('outline');
 
   const token = useSelector<RootState, string>(state => state.auth.token);
   if (token) {
@@ -49,6 +52,7 @@ const Join: React.FC = () => {
           password,
           name,
           email,
+          gender,
           user_type: UserType.Consumer,
           account_type: AccountType.Email
         };
@@ -61,81 +65,93 @@ const Join: React.FC = () => {
     }
   }
 
+  const onMaleClicked = () => {
+    setGender('Male');
+    setFemaleFill('outline');
+    setMaleFill('solid');
+  }
+
+  const onFemaleClicked = () => {
+    setGender('Female');
+    setMaleFill('outline');
+    setFemaleFill('solid');
+  }
+
   return (
     <IonPage>
-      <IonContent className="bg-img-join">
+      <IonContent className='bg-img-join'>
         <IonGrid>
           <IonRow>
-            <IonCol className="ion-text-center">
-              <IonImg src={icon} alt="topImgJoin" className="top-img-join" />
+            <IonCol className='ion-text-center'>
+              <IonImg src={icon} alt='topImgJoin' className='top-img-join' />
             </IonCol>
           </IonRow>
         </IonGrid>
 
-        <IonTitle className="ion-text-center" id="add-photo-txt">
+        <IonTitle className='ion-text-center' id='add-photo-txt'>
           Add Photo
         </IonTitle>
 
         <IonList className='bg-transparent'>
-          <IonItem color="transparent" className="name-item">
+          <IonItem color='transparent' className='name-item'>
             <IonInput
-              placeholder="Name"
-              className="name-txtbx"
-              type="text"
+              placeholder='Name'
+              className='name-txtbx'
+              type='text'
               onIonChange={(e) => setName(e.detail.value!)}
               value={name}
             />
           </IonItem>
-          <IonItem color="transparent" className="username-item">
+          <IonItem color='transparent' className='username-item'>
             <IonInput
-              placeholder="Username"
-              className="username-txtbx"
-              type="text"
+              placeholder='Username'
+              className='username-txtbx'
+              type='text'
               onIonChange={(e) => setUsername(e.detail.value!)}
               value={username}
             ></IonInput>
           </IonItem>
-          <IonItem color="transparent" className="email-item">
+          <IonItem color='transparent' className='email-item'>
             <IonInput
-              placeholder="Email Address"
-              className="email-txtbx"
-              type="email"
+              placeholder='Email Address'
+              className='email-txtbx'
+              type='email'
               onIonChange={(e) => {
                 setEmail(e.detail.value!);
               }}
               value={email}
             ></IonInput>
           </IonItem>
-          <IonItem color="transparent" className="password-item">
+          <IonItem color='transparent' className='password-item'>
             <IonInput
-              placeholder="Password"
-              className="password-txtbx"
-              type="password"
+              placeholder='Password'
+              className='password-txtbx'
+              type='password'
               onIonChange={(e) => setPassword(e.detail.value!)}
               value={password}
             />
           </IonItem>
-          <IonItem color="transparent" className="confirm-password-item">
+          <IonItem color='transparent' className='confirm-password-item'>
             <IonInput
-              placeholder="Confirm Password"
-              className="confirm-password-txtbx"
-              type="password"
+              placeholder='Confirm Password'
+              className='confirm-password-txtbx'
+              type='password'
               onIonChange={(e) => setConfirmPassword(e.detail.value!)}
               value={confirmPassword}
             ></IonInput>
           </IonItem>
-          <IonItem color="transparent" className="location-item">
+          <IonItem color='transparent' className='location-item'>
             <IonInput
-              placeholder="Location"
-              className="location-txtbx"
-              type="text"
+              placeholder='Location'
+              className='location-txtbx'
+              type='text'
             ></IonInput>
           </IonItem>
-          <IonItem color="transparent" className="location-item">
+          <IonItem color='transparent' className='location-item'>
             <IonInput
-              placeholder="Birthday"
-              className="location-txtbx"
-              type="text"
+              placeholder='Birthday'
+              className='location-txtbx'
+              type='text'
             ></IonInput>
           </IonItem>
         </IonList>
@@ -143,25 +159,27 @@ const Join: React.FC = () => {
           <IonRow>
             <IonCol>
               <IonButton
-                size="large"
-                expand="block"
+                size='large'
+                expand='block'
                 strong
-                fill="outline"
-                className="btn-join-joinpage"
+                fill={maleFill}
+                className='btn-join-joinpage'
+                onClick={() => onMaleClicked()}
               >
-                {" "}
+                {' '}
                     MALE
                   </IonButton>
             </IonCol>
             <IonCol>
               <IonButton
-                size="large"
-                expand="block"
+                size='large'
+                expand='block'
                 strong
-                fill="outline"
-                className="btn-join-joinpage"
+                fill={femaleFill}
+                className='btn-join-joinpage'
+                onClick={() => onFemaleClicked()}
               >
-                {" "}
+                {' '}
                     FEMALE
                   </IonButton>
             </IonCol>
@@ -170,21 +188,21 @@ const Join: React.FC = () => {
             <IonCol>
               <IonItem color='transparent' lines='none'>
                 <IonCheckbox className='cb-transparent' slot='start'></IonCheckbox>
-                <IonLabel>By accepting Terms of Use, I acknowledge that I am of legal drinking age</IonLabel>
+                <IonLabel className='ion-text-wrap'>By accepting Terms of Use, I acknowledge that I am of legal drinking age</IonLabel>
               </IonItem>
             </IonCol>
           </IonRow>
           <IonRow>
             <IonCol>
               <IonButton
-                size="large"
-                expand="block"
+                size='large'
+                expand='block'
                 strong
-                fill="outline"
-                className="btn-join-joinpage"
+                fill='outline'
+                className='btn-join-joinpage'
                 onClick={() => onJoin()}
               >
-                {" "}
+                {' '}
                   JOIN
                 </IonButton>
             </IonCol>
